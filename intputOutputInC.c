72a2af3baffe70dbb3124b70549e45373b70b618
hrsejz <muhammedharrisejaz@gmail.com>
2024-10-20 22:41:32 +0500

    uncommented some code

--- intputOutputInC.c
+++ intputOutputInC.c
@@ -3,26 +3,26 @@
 int main()
 {
     // scanner function
-    // int age;
-    // double weight;
-    // char grade;
+    int age;
+    double weight;
+    char grade;
 
-    // printf("Enter your age here: ");
-    // scanf("%d", &age);
-    // printf("Age = %d\n", age);
+    printf("Enter your age here: ");
+    scanf("%d", &age);
+    printf("Age = %d\n", age);
 
-    // printf("Enter your grade: ");
-    // scanf("\n");
-    // scanf("%c", &grade);
-    // printf("Grade = %c\n", grade);
+    printf("Enter your grade: ");
+    scanf("\n");
+    scanf("%c", &grade);
+    printf("Grade = %c\n", grade);
 
-    // printf("Enter your weight: ");
-    // scanf("%lf", &weight);
-    // printf("Weight = %.2lf\n", weight);
+    printf("Enter your weight: ");
+    scanf("%lf", &weight);
+    printf("Weight = %.2lf\n", weight);
 
     int a;
     char b;
-    printf("Enter an int and then a char,e.g. 8 B: ");
+    printf("Enter an int and then a char,e.g. 8: ");
     scanf("%i %c", &a, &b);
     printf("Interger: %i\n", a);
     printf("Character: %c",b);
